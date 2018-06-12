b0af7e61c97993dd39cd14c535683f8f91a95a65
ducanh <ducanhdhtb@gmail.com>
2018-06-12 13:59:37 +0700

    Redirect

---
+++ app/code/Excellence/Hello/Controller/Index/Index/ActionName.php
@@ -0,0 +1,14 @@
+<?php
+namespace Excellence\Hello\Controller\Index;
+use Magento\Framework\Controller\ResultFactory;
+class Actionname name extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        // Your code
+        $resultRedirect->setPath('excellence/index/add');
+        return $resultRedirect;
+    }
+}
+?>

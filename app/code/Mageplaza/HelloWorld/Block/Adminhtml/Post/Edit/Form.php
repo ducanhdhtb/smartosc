0f9e1a50a9eafb241ca76324894b77b269f8fb16
ducanh <ducanhdhtb@gmail.com>
2018-06-14 10:28:34 +0700

    Broken COmponent UI

--- app/code/Mageplaza/HelloWorld/Block/Adminhtml/Post/Edit/Form.php
+++ app/code/Mageplaza/HelloWorld/Block/Adminhtml/Post/Edit/Form.php
@@ -3,11 +3,27 @@
 namespace Mageplaza\HelloWorld\Block\Adminhtml\Post\Edit;
 
 use Magento\Backend\Block\Widget\Form\Generic;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\Data\FormFactory;
+use Mageplaza\HelloWorld\Model\Config\Status;
 
 class Form extends Generic
 {
+    protected $_postStatus;
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        FormFactory $formFactory,
+        Status $status,
+        array $data)
+    {
+        $this->_postStatus = $status;
+        parent::__construct($context, $registry, $formFactory, $data);
+    }
     protected function _prepareForm()
     {
+
         $model = $this->_coreRegistry->registry("staff");
         $form = $this->_formFactory->create(
             [
@@ -19,10 +35,18 @@ class Form extends Generic
                 ]
             ]
         );
-        $fieldset=$form->addFieldset(
+        $fieldset = $form->addFieldset(
             "base_fieldset",
-            ["legend"=>__("General Information"),"class"=>"fieldset-wide"]
+            ["legend" => __("General Information"), "class" => "fieldset-wide"]
         );
+
+        if($model->getId()){
+            $fieldset->addField(
+                "id",
+                "hidden",
+                ["name" => "id"]
+            );
+        }
 
         $fieldset->addField(
             "name",
@@ -65,24 +89,27 @@ class Form extends Generic
         );
         $fieldset->addField(
             "status",
-            "text",
+            "select",
             [
                 "name" => "name",
-                "label" => __("Selectbox"),
+                "label" => __("Status"),
                 "required" => true,
+                "options" => $this->_postStatus->toOptionArray(),
+
             ]
         );
 
         $fieldset->addField(
             "featured_image",
-            "text",
+            "image",
             [
                 "name" => "name",
                 "label" => __("Feature image"),
                 "required" => true,
             ]
         );
-
+        $data = $model->getData();
+        $form->setValues($data);
         $form->setHtmlIdPrefix("staff_main_");
         $form->setUseContainer(true);
         $this->setForm($form);

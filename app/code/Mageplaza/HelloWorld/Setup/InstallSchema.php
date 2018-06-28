194f75e39be1c1d84475e22df647c69596a65758
ducanh <ducanhdhtb@gmail.com>
2018-06-28 09:01:54 +0700

    customise module

--- app/code/Mageplaza/HelloWorld/Setup/InstallSchema.php
+++ app/code/Mageplaza/HelloWorld/Setup/InstallSchema.php
@@ -1,81 +1,97 @@
 <?php
 namespace Mageplaza\HelloWorld\Setup;
+
 class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
 {
+
     public function install(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
     {
         $installer = $setup;
         $installer->startSetup();
-        $table = $installer->getConnection()->newTable($installer->getTable('mageplaza_helloworld_post'));
-        $table->addColumn(
-            'post_id',
-            TYPE_INTEGER,
-            null,
-            [
-                'identity' => true,
-                'nullable' => false,
-                'primary' => true,
-                'unsigned' => true,
-            ])
-            ->addColumn(
-                'name',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                255,
-                ['nullable => false'],
-                'Post Name'
+        if (!$installer->tableExists('mageplaza_helloworld_post')) {
+            $table = $installer->getConnection()->newTable(
+                $installer->getTable('mageplaza_helloworld_post')
             )
-            ->addColumn(
-                'url_key',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                255,
-                [],
-                'Post URL Key'
-            )
-            ->addColumn(
-                'post_content',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                '64k',
-                [],
-                'Post Post Content'
-            )
-            ->addColumn(
-                'tags',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                255,
-                [],
-                'Post Tags'
-            )
-            ->addColumn(
-                'status',
-                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                1,
-                [],
-                'Post Status'
-            )
-            ->addColumn(
-                'featured_image',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                255,
-                [],
-                'Post Featured Image'
-            )
-            ->addColumn(
-                'created_at',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-                null,
-                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
-                'Created At'
-            )
-            ->addColumn(
-                'updated_at',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-                null,
-                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],)
+                ->addColumn(
+                    'post_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'nullable' => false,
+                        'primary'  => true,
+                        'unsigned' => true,
+                    ],
+                    'Post ID'
+                )
+                ->addColumn(
+                    'name',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    ['nullable => false'],
+                    'Post Name'
+                )
+                ->addColumn(
+                    'url_key',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'Post URL Key'
+                )
+                ->addColumn(
+                    'post_content',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    '64k',
+                    [],
+                    'Post Post Content'
+                )
+                ->addColumn(
+                    'tags',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'Post Tags'
+                )
+                ->addColumn(
+                    'status',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    1,
+                    [],
+                    'Post Status'
+                )
+                ->addColumn(
+                    'featured_image',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'Post Featured Image'
+                )
+                ->addColumn(
+                    'created_at',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                    null,
+                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+                    'Created At'
+                )->addColumn(
+                    'updated_at',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                    null,
+                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+                    'Updated At')
+                ->setComment('Post Table');
+            $installer->getConnection()->createTable($table);
 
-        $installer->getConnectiontion->createTable($table);
+            $installer->getConnection()->addIndex(
+                $installer->getTable('mageplaza_helloworld_post'),
+                $setup->getIdxName(
+                    $installer->getTable('mageplaza_helloworld_post'),
+                    ['name','url_key','post_content','tags','featured_image'],
+                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+                ),
+                ['name','url_key','post_content','tags','featured_image'],
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+        }
         $installer->endSetup();
-
-    };
-
-
+    }
 }
